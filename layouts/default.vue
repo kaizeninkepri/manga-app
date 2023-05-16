6343dc5f7e5a45bd2a54065a48f37ef4a4a7baac
Apple <apple@Apples-MacBook-Air.local>
2023-05-16 21:04:21 +0700

    modify default page (header)

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,28 +1,78 @@
 <template>
-    <div>
-        <v-layout>
-            <v-container>
-                <v-toolbar density="compact" class="rounded-lg mb-2 headerBg">
-                    <v-app-bar-nav-icon></v-app-bar-nav-icon>
+  <div>
+    <v-layout>
 
-                    <v-toolbar-title>Dailymanga.com
+      <v-app-bar
+        density="compact"
+        class="headerBg pa-4"
+        :elevation="0"
+      >
+        <!-- <template v-slot:prepend>
+          <v-app-bar-nav-icon></v-app-bar-nav-icon>
+        </template> -->
 
-                    </v-toolbar-title>
-                    <v-btn>Top Manga</v-btn>
-                    <v-btn>Lastest Update</v-btn>
-                    <v-btn>Recomendations</v-btn>
-                    <v-btn>Reviews</v-btn>
-                    <v-spacer></v-spacer>
+        <v-app-bar-title>Dailymangas.com</v-app-bar-title>
+        <v-spacer></v-spacer>
+        <v-text-field
+          density="compact"
+          variant="solo"
+          label="Search Manga"
+          append-inner-icon="mdi-magnify"
+          single-line
+          class="me-2"
+          hide-details
+        ></v-text-field>
+        <v-btn
+          class="me-2 text-none"
+          color="#4f545c"
+          prepend-icon="mdi-export-variant"
+          variant="flat"
+        >
+          login
+        </v-btn>
+        <v-btn
+          class="me-2 text-none"
+          color="#4f545c"
+          prepend-icon="mdi-export-variant"
+          variant="flat"
+        >
+          Register
+        </v-btn>
 
-                    <v-text-field density="compact" variant="solo" label="Search Manga" append-inner-icon="mdi-magnify"
-                        single-line hide-details></v-text-field>
-                </v-toolbar>
-                <v-main style="min-height: 300px;">
+        <v-btn icon>
+          <v-icon>mdi-heart</v-icon>
+        </v-btn>
+      </v-app-bar>
+      <!-- <v-toolbar
+        density="compact"
+        class="rounded-lg mb-2 headerBg"
+      >
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
 
-                    <slot />
-                </v-main>
-            </v-container>
+        <v-toolbar-title>Dailymanga.com
 
-        </v-layout>
-    </div>
+        </v-toolbar-title>
+        <v-btn>Top Manga</v-btn>
+        <v-btn>Lastest Update</v-btn>
+        <v-btn>Recomendations</v-btn>
+        <v-btn>Reviews</v-btn>
+        <v-spacer></v-spacer>
+
+        <v-text-field
+          density="compact"
+          variant="solo"
+          label="Search Manga"
+          append-inner-icon="mdi-magnify"
+          single-line
+          hide-details
+        ></v-text-field>
+      </v-toolbar> -->
+      <v-main style="min-height: 300px;">
+        <v-container fluid>
+          <slot />
+        </v-container>
+      </v-main>
+
+    </v-layout>
+  </div>
 </template>
